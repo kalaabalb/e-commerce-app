549de6caf9ba30b461056fe8523cb0276a839db4
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-30 00:03:11 +0300

    Create notification screen header

--- client_side/admin_panal_start/lib/screens/notification/components/notification_header.dart
+++ client_side/admin_panal_start/lib/screens/notification/components/notification_header.dart
@@ -1,6 +1,8 @@
+// lib/screens/notification/components/notification_header.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import '../../../utility/constants.dart';
+import '../../../widgets/responsive_header.dart';
+import 'package:admin_panal_start/utility/responsive_utils.dart';
 
 class NotificationHeader extends StatelessWidget {
   const NotificationHeader({
@@ -9,94 +11,12 @@ class NotificationHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          "Notification",
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-        Spacer(flex: 2),
-        Expanded(child: SearchField(
-          onChange: (val) {
-            //TODO: should complete call filterNotifications
-          },
-        )),
-        ProfileCard()
-      ],
-    );
-  }
-}
-
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: defaultPadding),
-      padding: EdgeInsets.symmetric(
-        horizontal: defaultPadding,
-        vertical: defaultPadding / 2,
-      ),
-      decoration: BoxDecoration(
-        color: secondaryColor,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-        border: Border.all(color: Colors.white10),
-      ),
-      child: Row(
-        children: [
-          Image.asset(
-            "assets/images/profile_pic.png",
-            height: 38,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-            child: Text("Angelina Jolie"),
-          ),
-          Icon(Icons.keyboard_arrow_down),
-        ],
-      ),
-    );
-  }
-}
-
-class SearchField extends StatelessWidget {
-  final Function(String) onChange;
-
-  const SearchField({
-    Key? key,
-    required this.onChange,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: "Search",
-        fillColor: secondaryColor,
-        filled: true,
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-        ),
-        suffixIcon: InkWell(
-          onTap: () {},
-          child: Container(
-            padding: EdgeInsets.all(defaultPadding * 0.75),
-            margin: EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-            decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-            ),
-            child: SvgPicture.asset("assets/icons/Search.svg"),
-          ),
-        ),
-      ),
-      onChanged: (value) {
-        onChange(value);
+    return ResponsiveHeader(
+      title: "Notification",
+      onSearch: (val) {
+        // TODO: should complete call filterNotifications
       },
+      actionButton: null,
     );
   }
 }
