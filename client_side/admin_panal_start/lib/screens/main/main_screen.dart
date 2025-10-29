de96acc869b3a60a91acf5ff5e78e9d5d43b1d31
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-30 00:03:11 +0300

    Build main app layout with navigation

--- client_side/admin_panal_start/lib/screens/main/main_screen.dart
+++ client_side/admin_panal_start/lib/screens/main/main_screen.dart
@@ -1,35 +1,43 @@
 import 'package:admin_panal_start/utility/constants.dart';
-import 'package:admin_panal_start/utility/responsive.dart';
-
+import 'package:admin_panal_start/utility/responsive_utils.dart';
 import 'provider/main_screen_provider.dart';
 import '../../utility/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'components/side_menu.dart';
 
-// Update MainScreen to be responsive
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Responsive.isMobile(context)
+      key: _scaffoldKey,
+      backgroundColor: bgColor,
+      appBar: ResponsiveUtils.isMobile(context)
           ? AppBar(
               title: Text('Admin Panel'),
               backgroundColor: secondaryColor,
               leading: IconButton(
                 icon: Icon(Icons.menu),
-                onPressed: () => Scaffold.of(context).openDrawer(),
+                onPressed: () => _scaffoldKey.currentState?.openDrawer(),
               ),
             )
           : null,
-      drawer: Responsive.isMobile(context) ? SideMenu() : null,
+      drawer: ResponsiveUtils.isMobile(context)
+          ? _buildMobileDrawer(context)
+          : null,
       body: SafeArea(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Side menu for desktop
-            if (!Responsive.isMobile(context)) Expanded(child: SideMenu()),
-            // Main content area
+            if (!ResponsiveUtils.isMobile(context))
+              _buildDesktopSidebar(context),
             Expanded(
               flex: 5,
               child: Consumer<MainScreenProvider>(
@@ -40,6 +48,30 @@ class MainScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildMobileDrawer(BuildContext context) {
+    return Drawer(
+      backgroundColor: secondaryColor,
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: SideMenu(
+        onItemSelected: () {
+          if (_scaffoldKey.currentState?.isDrawerOpen ?? false) {
+            Navigator.of(context).pop();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildDesktopSidebar(BuildContext context) {
+    return Container(
+      width: ResponsiveUtils.isTablet(context) ? 80 : 250,
+      child: SideMenu(
+        onItemSelected: () {},
+        compact: ResponsiveUtils.isTablet(context),
       ),
     );
   }
