00edd814c9e09677511c04e2f6512fa16866103d
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-30 00:03:15 +0300

    Add custom text input field

--- client_side/admin_panal_start/lib/widgets/custom_text_field.dart
+++ client_side/admin_panal_start/lib/widgets/custom_text_field.dart
@@ -1,6 +1,8 @@
+// lib/widgets/custom_text_field.dart - Updated
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../utility/constants.dart';
+import 'package:admin_panal_start/utility/responsive_utils.dart';
 
 class CustomTextField extends StatelessWidget {
   final String labelText;
@@ -9,6 +11,9 @@ class CustomTextField extends StatelessWidget {
   final int? lineNumber;
   final void Function(String?) onSave;
   final String? Function(String?)? validator;
+  final bool enabled;
+  final bool obscureText;
+  final Widget? suffixIcon;
 
   const CustomTextField({
     Key? key,
@@ -16,7 +21,11 @@ class CustomTextField extends StatelessWidget {
     required this.onSave,
     this.inputType = TextInputType.text,
     this.lineNumber = 1,
-    this.validator, required this.controller,
+    this.validator,
+    required this.controller,
+    this.enabled = true,
+    this.obscureText = false,
+    this.suffixIcon,
   }) : super(key: key);
 
   @override
@@ -26,12 +35,41 @@ class CustomTextField extends StatelessWidget {
       child: TextFormField(
         controller: controller,
         maxLines: lineNumber,
+        enabled: enabled,
+        obscureText: obscureText,
         decoration: InputDecoration(
           labelText: labelText,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: secondaryColor),
+          labelStyle: TextStyle(
+            color: Colors.grey.shade400,
+            fontSize: ResponsiveUtils.isMobile(context) ? 14 : 16,
           ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(color: Colors.grey.shade600),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(color: Colors.grey.shade600),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(color: primaryColor, width: 2),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(color: Colors.red.shade400),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(color: Colors.red.shade400, width: 2),
+          ),
+          filled: true,
+          fillColor: enabled ? Colors.transparent : Colors.grey.shade800,
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: ResponsiveUtils.isMobile(context) ? 12 : 16,
+          ),
+          suffixIcon: suffixIcon,
         ),
         keyboardType: inputType,
         onSaved: (value) {
@@ -40,8 +78,13 @@ class CustomTextField extends StatelessWidget {
         validator: validator,
         inputFormatters: [
           LengthLimitingTextInputFormatter(700),
-          if (inputType == TextInputType.number) FilteringTextInputFormatter.allow(RegExp(r'^-?\d*\.?\d*')),
+          if (inputType == TextInputType.number)
+            FilteringTextInputFormatter.allow(RegExp(r'^-?\d*\.?\d*')),
         ],
+        style: TextStyle(
+          fontSize: ResponsiveUtils.isMobile(context) ? 14 : 16,
+          color: enabled ? Colors.white : Colors.grey.shade400,
+        ),
       ),
     );
   }
