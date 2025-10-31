799ff3f0c1e923e8d8828f5d555edd5f04f771db
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:29 +0300

    Add profile management provider

--- client_side/flutter_ecommerce_new/lib/screen/profile_screen/provider/profile_provider.dart
+++ client_side/flutter_ecommerce_new/lib/screen/profile_screen/provider/profile_provider.dart
@@ -1,43 +1,112 @@
-import '../../../core/data/data_provider.dart';
-import '../../../utility/constants.dart';
-import '../../../utility/snack_bar_helper.dart';
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:get_storage/get_storage.dart';
+import '../../../utility/constants.dart';
 
 class ProfileProvider extends ChangeNotifier {
- final DataProvider _dataProvider;
- final box = GetStorage();
+  final GetStorage box = GetStorage();
 
- final GlobalKey<FormState> addressFormKey = GlobalKey<FormState>();
- TextEditingController phoneController = TextEditingController();
- TextEditingController streetController = TextEditingController();
- TextEditingController cityController = TextEditingController();
- TextEditingController stateController = TextEditingController();
- TextEditingController postalCodeController = TextEditingController();
- TextEditingController countryController = TextEditingController();
- TextEditingController couponController = TextEditingController();
+  // Address form controllers
+  final GlobalKey<FormState> addressFormKey = GlobalKey<FormState>();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController streetController = TextEditingController();
+  final TextEditingController cityController = TextEditingController();
+  final TextEditingController stateController = TextEditingController();
+  final TextEditingController postalCodeController = TextEditingController();
+  final TextEditingController countryController = TextEditingController();
 
- ProfileProvider(this._dataProvider) {
-  retrieveSavedAddress();
- }
+  String? _profileImagePath;
 
- void storeAddress() {
-  box.write(PHONE_KEY, phoneController.text);
-  box.write(STREET_KEY, streetController.text);
-  box.write(CITY_KEY, cityController.text);
-  box.write(STATE_KEY, stateController.text);
-  box.write(POSTAL_CODE_KEY, postalCodeController.text);
-  box.write(COUNTRY_KEY, countryController.text);
-  SnackBarHelper.showSuccessSnackBar('Address Stored Successfully');
- }
+  String? get profileImagePath => _profileImagePath;
 
-// Method to retrieve saved address values
- void retrieveSavedAddress() {
-  phoneController.text = box.read(PHONE_KEY) ?? '';
-  streetController.text = box.read(STREET_KEY) ?? '';
-  cityController.text = box.read(CITY_KEY) ?? '';
-  stateController.text = box.read(STATE_KEY) ?? '';
-  postalCodeController.text = box.read(POSTAL_CODE_KEY) ?? '';
-  countryController.text = box.read(COUNTRY_KEY) ?? '';
- }
+  ProfileProvider() {
+    _loadProfileImage();
+    retrieveSavedAddress();
+  }
+
+  void _loadProfileImage() {
+    _profileImagePath = box.read('profileImagePath');
+  }
+
+  Future<void> pickProfileImage() async {
+    try {
+      final ImagePicker picker = ImagePicker();
+      final XFile? image = await picker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 80,
+      );
+
+      if (image != null) {
+        final Directory appDir = await getApplicationDocumentsDirectory();
+        final String fileName =
+            'profile_image_${DateTime.now().millisecondsSinceEpoch}.jpg';
+        final String savePath = '${appDir.path}/$fileName';
+
+        final File savedImage = File(image.path).copySync(savePath);
+        _profileImagePath = savedImage.path;
+        box.write('profileImagePath', _profileImagePath);
+
+        notifyListeners();
+        Get.snackbar('Success', 'Profile picture updated successfully');
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to pick image: ${e.toString()}');
+    }
+  }
+
+  void clearProfileImage() {
+    _profileImagePath = null;
+    box.remove('profileImagePath');
+    notifyListeners();
+  }
+
+  // FIXED: Address management methods
+  void storeAddress() {
+    // Validate the form first
+    if (!addressFormKey.currentState!.validate()) {
+      return;
+    }
+
+    addressFormKey.currentState!.save();
+
+    box.write(PHONE_KEY, phoneController.text);
+    box.write(STREET_KEY, streetController.text);
+    box.write(CITY_KEY, cityController.text);
+    box.write(STATE_KEY, stateController.text);
+    box.write(POSTAL_CODE_KEY, postalCodeController.text);
+    box.write(COUNTRY_KEY, countryController.text);
+
+    Get.snackbar('Success', 'Address stored successfully');
+    notifyListeners();
+  }
+
+  void retrieveSavedAddress() {
+    phoneController.text = box.read(PHONE_KEY) ?? '';
+    streetController.text = box.read(STREET_KEY) ?? '';
+    cityController.text = box.read(CITY_KEY) ?? '';
+    stateController.text = box.read(STATE_KEY) ?? '';
+    postalCodeController.text = box.read(POSTAL_CODE_KEY) ?? '';
+    countryController.text = box.read(COUNTRY_KEY) ?? '';
+  }
+
+  void clearAddress() {
+    phoneController.clear();
+    streetController.clear();
+    cityController.clear();
+    stateController.clear();
+    postalCodeController.clear();
+    countryController.clear();
+
+    box.remove(PHONE_KEY);
+    box.remove(STREET_KEY);
+    box.remove(CITY_KEY);
+    box.remove(STATE_KEY);
+    box.remove(POSTAL_CODE_KEY);
+    box.remove(COUNTRY_KEY);
+
+    notifyListeners();
+  }
 }
