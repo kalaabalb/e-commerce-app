8cc7b04326eef55e12b737144c202f2d16a6627f
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:30 +0300

    Add context extension helpers

--- client_side/flutter_ecommerce_new/lib/utility/extensions.dart
+++ client_side/flutter_ecommerce_new/lib/utility/extensions.dart
@@ -1,26 +1,56 @@
-import '../screen/login_screen/provider/user_provider.dart';
-import '../screen/product_cart_screen/provider/cart_provider.dart';
-import '../screen/profile_screen/provider/profile_provider.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../core/data/data_provider.dart';
+import '../screen/login_screen/provider/user_provider.dart';
+import '../screen/profile_screen/provider/profile_provider.dart';
+import '../screen/product_list_screen/provider/product_list_provider.dart';
 import '../screen/product_by_category_screen/provider/product_by_category_provider.dart';
 import '../screen/product_details_screen/provider/product_detail_provider.dart';
+import '../screen/product_cart_screen/provider/cart_provider.dart';
 import '../screen/product_favorite_screen/provider/favorite_provider.dart';
 
-extension Providers on BuildContext {
-  DataProvider get dataProvider => Provider.of<DataProvider>(this, listen: false);
-  ProductByCategoryProvider get proByCProvider => Provider.of<ProductByCategoryProvider>(this, listen: false);
-  ProductDetailProvider get proDetailProvider => Provider.of<ProductDetailProvider>(this, listen: false);
-  CartProvider get cartProvider => Provider.of<CartProvider>(this, listen: false);
-  FavoriteProvider get favoriteProvider => Provider.of<FavoriteProvider>(this, listen: false);
-  UserProvider get userProvider => Provider.of<UserProvider>(this, listen: false);
-  ProfileProvider get profileProvider => Provider.of<ProfileProvider>(this, listen: false);
+extension DataProviderExtension on BuildContext {
+  DataProvider get dataProvider => read<DataProvider>();
 }
 
+extension UserProviderExtension on BuildContext {
+  UserProvider get userProvider => read<UserProvider>();
+}
 
+extension ProfileProviderExtension on BuildContext {
+  ProfileProvider get profileProvider => read<ProfileProvider>();
+}
 
+extension ProductListProviderExtension on BuildContext {
+  ProductListProvider get productListProvider => read<ProductListProvider>();
+}
 
+extension ProductByCategoryProviderExtension on BuildContext {
+  ProductByCategoryProvider get proByCProvider =>
+      read<ProductByCategoryProvider>();
+}
 
+extension ProductDetailProviderExtension on BuildContext {
+  ProductDetailProvider get proDetailProvider => read<ProductDetailProvider>();
+}
 
+extension CartProviderExtension on BuildContext {
+  CartProvider get cartProvider => read<CartProvider>();
+}
+
+extension FavoriteProviderExtension on BuildContext {
+  FavoriteProvider get favoriteProvider => read<FavoriteProvider>();
+}
+
+extension SafeDataProviderExtension on BuildContext {
+  DataProvider get safeDataProvider {
+    try {
+      return read<DataProvider>();
+    } catch (e) {
+      // Return a mock or handle the error appropriately
+      throw FlutterError(
+        'DataProvider not found in context. Make sure it\'s provided above in the widget tree.',
+      );
+    }
+  }
+}
