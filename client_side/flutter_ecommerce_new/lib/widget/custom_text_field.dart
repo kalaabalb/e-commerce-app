7786d1243032f2c80f2db30ffdf864bb75ec2e73
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:31 +0300

    Implement custom text field

--- client_side/flutter_ecommerce_new/lib/widget/custom_text_field.dart
+++ client_side/flutter_ecommerce_new/lib/widget/custom_text_field.dart
@@ -1,6 +1,6 @@
+// lib/widget/custom_text_field.dart
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import '../utility/app_color.dart';
 
 class CustomTextField extends StatelessWidget {
@@ -11,6 +11,9 @@ class CustomTextField extends StatelessWidget {
   final int? lineNumber;
   final void Function(String?) onSave;
   final String? Function(String?)? validator;
+  final bool obscureText;
+  final TextAlign textAlign;
+  final TextDirection? textDirection;
 
   const CustomTextField({
     super.key,
@@ -18,9 +21,13 @@ class CustomTextField extends StatelessWidget {
     required this.onSave,
     this.inputType = TextInputType.text,
     this.lineNumber = 1,
-    this.validator, required this.controller, this.height,
+    this.validator,
+    required this.controller,
+    this.height,
+    this.obscureText = false,
+    this.textAlign = TextAlign.left,
+    this.textDirection = TextDirection.ltr, // Force LTR typing
   });
-
 
   @override
   Widget build(BuildContext context) {
@@ -28,25 +35,33 @@ class CustomTextField extends StatelessWidget {
       height: height,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: TextFormField(
-          controller: controller,
-          maxLines: lineNumber,
-          decoration: InputDecoration(
-            labelText: labelText,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-              borderSide: const BorderSide(color: AppColor.darkOrange),
+        child: Directionality(
+          textDirection: TextDirection.ltr, // Force LTR for all text fields
+          child: TextFormField(
+            controller: controller,
+            maxLines: lineNumber,
+            obscureText: obscureText,
+            textAlign: TextAlign.left, // Force left alignment
+            textDirection: TextDirection.ltr, // Force LTR typing
+            decoration: InputDecoration(
+              labelText: labelText,
+              labelStyle: const TextStyle(),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                borderSide: const BorderSide(color: AppColor.darkOrange),
+              ),
             ),
+            keyboardType: inputType,
+            onSaved: (value) {
+              onSave(value?.isEmpty ?? true ? null : value);
+            },
+            validator: validator,
+            inputFormatters: [
+              LengthLimitingTextInputFormatter(700),
+              if (inputType == TextInputType.number)
+                FilteringTextInputFormatter.allow(RegExp(r'^-?\d*\.?\d*')),
+            ],
           ),
-          keyboardType: inputType,
-          onSaved: (value) {
-            onSave(value?.isEmpty ?? true ? null : value);
-          },
-          validator: validator,
-          inputFormatters: [
-            LengthLimitingTextInputFormatter(700),
-            if (inputType == TextInputType.number) FilteringTextInputFormatter.allow(RegExp(r'^-?\d*\.?\d*')),
-          ],
         ),
       ),
     );
