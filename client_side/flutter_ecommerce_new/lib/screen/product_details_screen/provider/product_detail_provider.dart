93b70e969e2c40cd8be8b1d0fcbc4a9c29b69903
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:28 +0300

    Add product details provider

--- client_side/flutter_ecommerce_new/lib/screen/product_details_screen/provider/product_detail_provider.dart
+++ client_side/flutter_ecommerce_new/lib/screen/product_details_screen/provider/product_detail_provider.dart
@@ -1,38 +1,60 @@
 import 'package:e_commerce_flutter/models/product.dart';
+import 'package:e_commerce_flutter/screen/product_cart_screen/provider/cart_provider.dart';
 import 'package:e_commerce_flutter/utility/snack_bar_helper.dart';
 import 'package:e_commerce_flutter/utility/utility_extention.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_cart/flutter_cart.dart';
-import '../../../core/data/data_provider.dart';
+import 'package:flutter_cart/model/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailProvider extends ChangeNotifier {
-  final DataProvider _dataProvider;
   String? selectedVariant;
-  var flutterCart = FlutterCart();
 
-  ProductDetailProvider(this._dataProvider);
+  void addToCart(Product product, BuildContext context) {
+    print('🟡 Adding to cart: ${product.name}, variant: $selectedVariant');
 
-  void addToCart(Product product) {
     if (product.proVariantId!.isNotEmpty && selectedVariant == null) {
-      SnackBarHelper.showSuccessSnackBar('select a variant');
+      SnackBarHelper.showErrorSnackBar('Please select a variant');
       return;
     }
+
     double? price = product.offerPrice != product.price
         ? product.offerPrice
         : product.price;
 
-    flutterCart.addToCart(
-      cartModel: CartModel(
-        productId: '${product.sId}',
-        productName: '${product.name}',
-        productImages: ['${product.images.safeElementAt(0)?.url}'],
-        variants: [ProductVariant(price: price ?? 0, color: selectedVariant)],
-        productDetails: '${product.description}',
-      ),
-    );
-    selectedVariant = null;
-    SnackBarHelper.showSuccessSnackBar('Item added');
-    notifyListeners();
+    try {
+      // Use CartProvider instead of local flutterCart
+      final cartProvider = Provider.of<CartProvider>(context, listen: false);
+
+      // Use the CartProvider's addToCart method
+      cartProvider.addToCart(
+        productId: product.sId!,
+        productName: product.name!,
+        price: price?.toDouble() ?? 0.0,
+        productImages:
+            product.images?.map((img) => img.url ?? '').toList() ?? [],
+        variants: [
+          ProductVariant(
+            price: price?.toDouble() ?? 0.0,
+            color: selectedVariant,
+          ),
+        ],
+        quantity: 1,
+      );
+
+      selectedVariant = null;
+      SnackBarHelper.showSuccessSnackBar('Item added to cart');
+
+      // Print cart contents for debugging
+      print('🟡 Cart contents after add:');
+      for (var item in cartProvider.myCartItems) {
+        print('  - ${item.productName} (${item.quantity})');
+      }
+
+      notifyListeners();
+    } catch (e) {
+      print('🔴 Error adding to cart: $e');
+      SnackBarHelper.showErrorSnackBar('Failed to add item to cart: $e');
+    }
   }
 
   void updateUI() {
