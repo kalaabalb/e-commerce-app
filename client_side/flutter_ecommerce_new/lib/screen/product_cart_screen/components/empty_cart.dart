e8ce310a02e15d2f456970a799af6a733dc379be
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:28 +0300

    Add empty cart state component

--- client_side/flutter_ecommerce_new/lib/screen/product_cart_screen/components/empty_cart.dart
+++ client_side/flutter_ecommerce_new/lib/screen/product_cart_screen/components/empty_cart.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerce_flutter/utility/extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../core/data/data_provider.dart';
 
 class EmptyCart extends StatelessWidget {
   const EmptyCart({super.key});
@@ -7,16 +10,33 @@ class EmptyCart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: Center(
-              child: Image.asset('assets/images/empty_cart.png'),
+          Icon(
+            Icons.shopping_cart_outlined,
+            size: 100,
+            color: Theme.of(
+              context,
+            ).textTheme.bodyLarge?.color?.withOpacity(0.3),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            context.dataProvider.translate('empty_cart'),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Theme.of(context).textTheme.bodyLarge?.color,
             ),
           ),
-          const Text(
-            "Empty cart",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          )
+          const SizedBox(height: 10),
+          Text(
+            'Your cart is empty',
+            style: TextStyle(
+              color: Theme.of(
+                context,
+              ).textTheme.bodyLarge?.color?.withOpacity(0.6),
+            ),
+          ),
         ],
       ),
     );
