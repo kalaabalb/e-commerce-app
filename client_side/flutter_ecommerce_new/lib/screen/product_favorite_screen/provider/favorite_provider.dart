0a04d99efafabe214fe197bdd48223860ac3b33d
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:28 +0300

    Implement favorites provider

--- client_side/flutter_ecommerce_new/lib/screen/product_favorite_screen/provider/favorite_provider.dart
+++ client_side/flutter_ecommerce_new/lib/screen/product_favorite_screen/provider/favorite_provider.dart
@@ -1,29 +1,23 @@
-import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
-
 import 'package:e_commerce_flutter/utility/constants.dart';
-
-import '../../../core/data/data_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get_storage/get_storage.dart';
 import '../../../models/product.dart';
 
 class FavoriteProvider extends ChangeNotifier {
-  final DataProvider _dataProvider;
   final box = GetStorage();
   List<Product> favoriteProduct = [];
-  FavoriteProvider(this._dataProvider);
 
-  updateToFavoriteList(String productId) {
+  FavoriteProvider();
+
+  void updateToFavoriteList(String productId) {
     List<dynamic> favoriteList = box.read(FAVORITE_PRODUCT_BOX) ?? [];
     if (favoriteList.contains(productId)) {
       favoriteList.remove(productId);
     } else {
       favoriteList.add(productId);
     }
-    checkIsItemFavorite(productId);
     box.write(FAVORITE_PRODUCT_BOX, favoriteList);
-    loadFavoriteItems();
-    notifyListeners();
+    notifyListeners(); // This will update the UI immediately
   }
 
   bool checkIsItemFavorite(String productId) {
@@ -32,14 +26,17 @@ class FavoriteProvider extends ChangeNotifier {
     return isExist;
   }
 
-  void loadFavoriteItems() {
+  void loadFavoriteItems(List<Product> allProducts) {
     List<dynamic> favoriteListIds = box.read(FAVORITE_PRODUCT_BOX) ?? [];
-    favoriteProduct = _dataProvider.products.where((product) {
+    favoriteProduct = allProducts.where((product) {
       return favoriteListIds.contains(product.sId);
     }).toList();
+    Future.microtask(() => notifyListeners());
   }
 
-  clearFavoriteList() {
+  void clearFavoriteList() {
     box.remove(FAVORITE_PRODUCT_BOX);
+    favoriteProduct.clear();
+    notifyListeners();
   }
 }
