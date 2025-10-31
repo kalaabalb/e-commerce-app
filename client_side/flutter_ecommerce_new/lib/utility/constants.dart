b689bd70ee2fdca8f0749a0dd020247d7edc4109
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:29 +0300

    Define app constants

--- client_side/flutter_ecommerce_new/lib/utility/constants.dart
+++ client_side/flutter_ecommerce_new/lib/utility/constants.dart
@@ -1,11 +1,17 @@
-const MAIN_URL = 'http://localhost:3000';
+const MAIN_URL = 'http://10.161.175.199:3000';
+
+// OR for development, you can use this dynamic approach:
+// const MAIN_URL = 'http://10.161.170.81:3000'; // Old IP - WRONG
+// const MAIN_URL = 'http://localhost:3000'; // For emulator only
 
 const FAVORITE_PRODUCT_BOX = 'FAVORITE_PRODUCT_BOX';
 const USER_INFO_BOX = 'USER_INFO_BOX';
 
-const  PHONE_KEY = 'PHONE_KEY';
-const  STREET_KEY = 'STREET_KEY';
-const  CITY_KEY = 'CITY_KEY';
-const  STATE_KEY = 'STATE_KEY';
-const  POSTAL_CODE_KEY = 'POSTAL_CODE_KEY';
-const  COUNTRY_KEY = 'COUNTRY_KEY';
+const PHONE_KEY = 'PHONE_KEY';
+const STREET_KEY = 'STREET_KEY';
+const CITY_KEY = 'CITY_KEY';
+const STATE_KEY = 'STATE_KEY';
+const POSTAL_CODE_KEY = 'POSTAL_CODE_KEY';
+const COUNTRY_KEY = 'COUNTRY_KEY';
+
+// In your extensions.dart file, add this:
