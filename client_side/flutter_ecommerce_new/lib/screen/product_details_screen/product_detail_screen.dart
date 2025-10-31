0c6d2ff703e3768a0d753c52500ebb5ef5fcf914
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:28 +0300

    Implement product details screen

--- client_side/flutter_ecommerce_new/lib/screen/product_details_screen/product_detail_screen.dart
+++ client_side/flutter_ecommerce_new/lib/screen/product_details_screen/product_detail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:e_commerce_flutter/utility/extensions.dart';
-
 import 'provider/product_detail_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,8 +15,10 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final proDetailProvider = context.proDetailProvider;
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+
     return SafeArea(
       child: Scaffold(
         extendBodyBehindAppBar: true,
@@ -34,7 +35,7 @@ class ProductDetailScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                //? product image section
+                // Product image section
                 Container(
                   height: height * 0.42,
                   width: width,
@@ -53,16 +54,16 @@ class ProductDetailScreen extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      //? product nam e
+                      // Product name
                       Text(
                         '${product.name}',
                         style: Theme.of(context).textTheme.displayMedium,
                       ),
                       const SizedBox(height: 10),
-                      //? product rating section
+                      // Product rating section
                       const ProductRatingSection(),
                       const SizedBox(height: 10),
-                      //? product rate , offer , stock section
+                      // Product rate, offer, stock section
                       Row(
                         children: [
                           Text(
@@ -86,8 +87,10 @@ class ProductDetailScreen extends StatelessWidget {
                           const Spacer(),
                           Text(
                             product.quantity != 0
-                                ? "Available stock : ${product.quantity}"
-                                : "Not available",
+                                ? "${context.dataProvider.translate('available_stock')} : ${product.quantity}"
+                                : context.dataProvider.translate(
+                                    'not_available',
+                                  ),
                             style: const TextStyle(fontWeight: FontWeight.w500),
                           ),
                         ],
@@ -95,7 +98,7 @@ class ProductDetailScreen extends StatelessWidget {
                       const SizedBox(height: 30),
                       product.proVariantId!.isNotEmpty
                           ? Text(
-                              'Available ${product.proVariantTypeId?.type}',
+                              '${context.dataProvider.translate('available')} ${product.proVariantTypeId?.type ?? context.dataProvider.translate('options')}',
                               style: const TextStyle(
                                 color: Colors.red,
                                 fontSize: 16,
@@ -115,26 +118,26 @@ class ProductDetailScreen extends StatelessWidget {
                           );
                         },
                       ),
-                      //? product description
+                      // Product description
                       Text(
-                        "About",
+                        context.dataProvider.translate('about'),
                         style: Theme.of(context).textTheme.headlineMedium,
                       ),
                       const SizedBox(height: 10),
                       Text("${product.description}"),
                       const SizedBox(height: 40),
-                      //? add to cart button
+                      // Add to cart button
                       SizedBox(
                         width: double.infinity,
                         child: ElevatedButton(
                           onPressed: product.quantity != 0
                               ? () {
-                                  context.proDetailProvider.addToCart(product);
+                                  proDetailProvider.addToCart(product, context);
                                 }
                               : null,
-                          child: const Text(
-                            "Add to cart",
-                            style: TextStyle(color: Colors.white),
+                          child: Text(
+                            context.dataProvider.translate('add_to_cart'),
+                            style: const TextStyle(color: Colors.white),
                           ),
                         ),
                       ),
