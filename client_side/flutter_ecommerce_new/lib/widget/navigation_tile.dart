b86027c67e74f51beef7a9c3cf9a34fcc6704abd
kalaabalb <alebachewkalaab99@gmail.com>
2025-10-31 21:26:31 +0300

    Implement navigation list tile

--- client_side/flutter_ecommerce_new/lib/widget/navigation_tile.dart
+++ client_side/flutter_ecommerce_new/lib/widget/navigation_tile.dart
@@ -1,44 +1,48 @@
 import 'package:flutter/material.dart';
-
 import '../utility/app_color.dart';
 
 class NavigationTile extends StatelessWidget {
   final IconData icon;
   final String title;
 
-  const NavigationTile({
-    super.key,
-    required this.icon,
-    required this.title,
-  });
+  const NavigationTile({super.key, required this.icon, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 5),
       child: Material(
-        color: AppColor.lightGrey,
+        color: Theme.of(context).cardColor, // Use theme card color
         borderRadius: BorderRadius.circular(10),
+        elevation: 2,
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
           child: Row(
             children: [
               Container(
-                padding: const EdgeInsets.all(5),
+                padding: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
                   color: AppColor.darkOrange,
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: Icon(icon, size: 30, color: Colors.white),
+                child: Icon(icon, size: 24, color: Colors.white),
               ),
               const SizedBox(width: 20),
               Expanded(
                 child: Text(
                   title,
-                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.black87),
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Theme.of(context).textTheme.bodyLarge?.color,
+                  ),
                 ),
               ),
-              const Icon(Icons.arrow_forward_ios, color: Colors.black87),
+              Icon(
+                Icons.arrow_forward_ios,
+                color: Theme.of(context).textTheme.bodyLarge?.color,
+                size: 16,
+              ),
             ],
           ),
         ),
